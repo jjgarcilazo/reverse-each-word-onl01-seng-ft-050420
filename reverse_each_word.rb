cb80cc75fbbf0618a0a25303a64e21298e161c3b
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-06 22:16:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,7 @@
-reverse_each_word("Hello there, and hw are you!")
+def reverse_each_word(sentence1)
+  split_array = sentence1.split
+  reversed_array = split_array.reverse
+  reversed_array.each do |joined_array|
+    joined_array.join(' ')
+  end
+end
